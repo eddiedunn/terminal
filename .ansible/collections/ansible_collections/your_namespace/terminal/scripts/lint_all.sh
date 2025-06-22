b4758f7bce92a2b455fbd8dfb80b7b4893afeeff
Eddie Dunn <gdunn6@oreillyauto.com>
2025-06-21 21:12:21 -0400

    feat: initialize terminal collection with ansible dependencies and modules

--- .ansible/collections/ansible_collections/your_namespace/terminal/scripts/lint_all.sh
+++ .ansible/collections/ansible_collections/your_namespace/terminal/scripts/lint_all.sh
@@ -10,6 +10,10 @@ fi
 find "$(dirname "$0")/.." \
   -type f \
   \( -name '*.yml' -o -name '*.yaml' \) \
+  ! -path '*/.venv/*' \
+  ! -path '*/site-packages/*' \
+  ! -path '*/__pycache__/*' \
+  ! -path '*/.mypy_cache/*' \
   ! -path '*/molecule/*/verify.yml' \
   ! -path '*/molecule/*/converge.yml' \
   -exec ansible-lint {} +
---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [1.0.0] - 2025-06-21
+### Added
+- Initial release of the modular terminal Ansible collection.
